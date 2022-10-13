eb99be4989ee50939ffc3aac5e27578418da2de0
Humberto Marcone <humberto.freitas@ionsistemas.com.br>
2022-10-13 14:56:00 -0300

    Subindo o projeto back-end
    Subindo o projeto back-end

---
+++ back-end/Domain/Interfaces/InterfaceServices/IServiceMessage.cs
@@ -0,0 +1,13 @@
+﻿using Entities.Entities;
+
+namespace Domain.Interfaces.InterfaceServices;
+
+public interface IServiceMessage
+{
+    Task Adicionar(Message Objeto);
+
+    Task Atualizar(Message Objeto);
+
+    Task<List<Message>> ListarMessageAtivas();
+
+}
---
+++ back-end/Entities/Enums/TipoUsuario.cs
@@ -0,0 +1,7 @@
+﻿namespace Entities.Enums;
+
+public enum TipoUsuario
+{
+    Administrador = 1, 
+    Comum = 2
+}
